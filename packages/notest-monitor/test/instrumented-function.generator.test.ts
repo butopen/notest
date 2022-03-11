b952923ff0418a7d46c676d87010f386245b5ac3
salvatoreromeo <info@salvatoreromeo.com>
2022-03-11 13:04:31 +0100

    initial import

---
+++ packages/notest-monitor/test/instrumented-function.generator.test.ts
@@ -0,0 +1,29 @@
+import {Project} from "ts-morph";
+
+test("test function is generated correctly", async () => {
+    /*
+    const functionCode = `
+    
+    function add(x:number, y:number) {
+        return x + y
+    }
+    
+    `
+
+    const project = new Project({
+        tsConfigFilePath: "tsconfig.json",
+    });
+    
+    const functionSource = project.createSourceFile("test.ts", functionCode)
+    
+    
+    const functionInstrumenter = new FunctionIntrumenter()
+    
+    const result = functionInstrumenter.instrument(functionSource)
+    
+    expect(result.functionName).toEqual("add")
+    expect(result.parameters.map(p => p.name).join(",")).toEqual("x,y")
+    expect(result.parameters.map(p => p.type).join(",")).toEqual("number,number")
+    
+     */
+})
