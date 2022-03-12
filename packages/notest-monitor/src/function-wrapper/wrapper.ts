ecd479816797637f3892247b33471a1b427b2a1d
Emanuele <emanuele.antimiani@gmail.com>
2022-03-12 15:24:54 +0100

    created new Class FunctionInstrumenter and methods backbone

--- packages/notest-monitor/src/function-wrapper/wrapper.ts
+++ packages/notest-monitor/src/function-wrapper/wrapper.ts
@@ -1,6 +1,6 @@
 import {
   FunctionDeclaration,
-  Project
+  Project, SyntaxKind
 } from "ts-morph";
 
 import * as fs from "fs"
@@ -96,5 +96,60 @@ export class Wrapper{
 
   private static setReturn(wrapFunc: FunctionDeclaration) {
     wrapFunc.addStatements('return output')
+  }
+}
+
+export class FunctionInstrumenter{
+  private project: Project;
+
+  constructor() {
+    this.project = new Project({
+      tsConfigFilePath: "tsconfig.json",
+    });
+  }
+
+  instrument(sourceFilePath: string, functionName: string){
+    const {sourceFile, sourceFunction, wrapFile, wrapFunction} = this.initialize(sourceFilePath, functionName)
+    this.setParameters(sourceFunction, wrapFunction)
+    sourceFunction.getStatements().forEach( statement => {
+      switch (statement.getKind()) {
+        case SyntaxKind.VariableStatement:
+          // Instrument Variable Statement
+          break
+        case SyntaxKind.ReturnStatement:
+          // Instrument Return Statement
+          break
+      }
+    })
+    return wrapFunction
+  }
+
+  private initialize(sourceFilePath: string, functionName: string) {
+    const sourceFile = this.project.getSourceFileOrThrow(sourceFilePath)
+    const sourceFunction = sourceFile.getFunctionOrThrow(functionName)
+
+    const pathWrapFile = `${sourceFile.getDirectoryPath()}/instrumentation/${sourceFile.getBaseName()}`
+    const wrapFile = this.project.createSourceFile(pathWrapFile)
+
+    if (wrapFile.getFunction(functionName)) {
+      throw new Error('Exit because case function already exist not handled (Work in progress)')
+    }
+
+    const wrapFunction = wrapFile.addFunction({name: functionName, isExported: true})
+    return {sourceFile, sourceFunction, wrapFile, wrapFunction}
+  }
+
+  private setParameters(func: FunctionDeclaration, wrapFunc: FunctionDeclaration){
+    const parameters:{name:string, type:string}[] = []
+
+    func.getParameters().forEach(param => {
+      parameters.push({
+        name: param.getName(),
+        type: param.getType().getText()
+      })
+    })
+
+    wrapFunc.addParameters(parameters)
+    return parameters;
   }
 }
