ca1b382d8097ac38e8be672cae96e9edea78f7fc
Emanuele <emanuele.antimiani@gmail.com>
2022-03-17 11:22:07 +0100

    added possibility to save informations about all expression

--- packages/notest-monitor/src/function-wrapper/statements_instrumenters/expression_instrumenter.ts
+++ packages/notest-monitor/src/function-wrapper/statements_instrumenters/expression_instrumenter.ts
@@ -6,16 +6,27 @@ export class ExpressionInstrumenter implements InstrumentStatementInterface {
 
   addCollector(statement: Statement | Expression) {
     const expressionStatement: ExpressionStatement = statement.asKindOrThrow(SyntaxKind.ExpressionStatement)
-    const variableToCollect = expressionStatement.getFirstDescendantByKindOrThrow(SyntaxKind.Identifier).getText()
-    statement.replaceWithText(writer =>
-      writer
-        .writeLine(statement.getFullText()).newLine()
-        .write(
-          InfoAdderForCollector.addInfo(
-            variableToCollect,
-            "expression",
-            expressionStatement.getStartLineNumber())
-        )
+    const expression = expressionStatement.getExpression().getText(false)
+    statement.replaceWithText(writer => {
+        writer
+          .writeLine(statement.getFullText()).newLine()
+          .write(
+            InfoAdderForCollector.addInfo(
+              `'${expression}'`,
+              "expression",
+              expressionStatement.getStartLineNumber())
+          )
+        if (expressionStatement.getChildren()[0].getKind() == SyntaxKind.BinaryExpression) {
+          const variableToCollect = expressionStatement.getFirstDescendantByKindOrThrow(SyntaxKind.Identifier).getText()
+          writer.newLine()
+            .write(
+              InfoAdderForCollector.addInfo(
+                variableToCollect,
+                "variable",
+                expressionStatement.getStartLineNumber())
+            )
+        }
+      }
     )
   }
 }
