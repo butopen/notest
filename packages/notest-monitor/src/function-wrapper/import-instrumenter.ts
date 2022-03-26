18053cf706071c700e8ca5f088fc675a4cc9c20b
Emanuele <emanuele.antimiani@gmail.com>
2022-03-26 11:45:35 +0100

    changed method destinationImportsChanger

--- packages/notest-monitor/src/function-wrapper/import-instrumenter.ts
+++ packages/notest-monitor/src/function-wrapper/import-instrumenter.ts
@@ -1,48 +1,34 @@
-import {FunctionDeclaration, SourceFile} from "ts-morph";
-import * as path from "path";
+import {FunctionDeclaration, SourceFile, SyntaxKind} from "ts-morph";
 
 export class ImportInstrumenter {
 
   destinationImportsChanger(sourceFunction: FunctionDeclaration, wrapFunction: FunctionDeclaration) {
     const sourceFunctionName = sourceFunction.getName()
     const sourceFileName = sourceFunction.getSourceFile().getBaseNameWithoutExtension()
-    //filter original file and wrapped file from references
-    const externalReferences = sourceFunction.findReferences()
-      .filter(ref =>
-        !(ref.getReferences()[0].getSourceFile().getFilePath() == sourceFunction.getSourceFile().getFilePath()
-          || ref.getReferences()[0].getSourceFile().getFilePath() == wrapFunction.getSourceFile().getFilePath()))
+    let imports = sourceFunction.findReferencesAsNodes()
+      .map(value => value.getFirstAncestor(node => node.getKind() == SyntaxKind.ImportDeclaration))
+      .filter(value => value)
+      .filter(value =>
+        value!.getSourceFile().getFilePath() != sourceFunction.getSourceFile().getFilePath()
+        && value!.getSourceFile().getFilePath() != wrapFunction.getSourceFile().getFilePath())
+      .map(value => value!.asKindOrThrow(SyntaxKind.ImportDeclaration)) //restituisce sempre un unico import per file?
 
-    //get sourceFiles
-    const externalReferencesSourceFile = externalReferences.map(ref => ref.getReferences()[0].getSourceFile())
-
-    externalReferencesSourceFile.forEach(file => {
-      let arrayImportRef = file.getImportDeclarations()
-        .filter(imp =>
-          path.resolve(imp.getModuleSpecifierValue())
-            .replace(/\\/g, '/') == sourceFunction.getSourceFile().getFilePath().replace(/\\/g, '/'))[0]
-      if (arrayImportRef) {
-        arrayImportRef.getImportClause()!.getNamedImports().forEach(
-          name => {
-            if (name.getName() != sourceFunctionName) {
-              //rewrite imports of other functions imported from same file
-              file.insertStatements(0, `import { ${name.getText()} } from '${arrayImportRef.getModuleSpecifierValue()}'`)
-            } else {
-              //replace import of function with instrumented one
-              let arrayElementPath = arrayImportRef.getModuleSpecifier()!
-                .getText()
-                .replace(/\\/g, '/')
-                .split('/')
-                .slice(0, -1)
-              arrayElementPath.push(`instrumentation/${sourceFileName}`)
-              let newPath = arrayElementPath.join('/')
-              file.insertStatements(0, `import { ${sourceFunctionName}Instrumented as ${sourceFunctionName} } from '${newPath.slice(1)}'`)
-            }
+    imports.forEach(imp => {
+      imp.getImportClause()!.getNamedImports().forEach(
+        name => {
+          if (name.getName() != sourceFunctionName) {
+            //rewrite imports of other functions imported from same file
+            imp.getSourceFile().insertStatements(0, `import { ${name.getText()} } from '${imp.getModuleSpecifierValue()}'`)
+          } else {
+            //replace import of function with instrumented one
+            let arrayElementPath = imp.getModuleSpecifier()!.getText().replace(/\\/g, '/')
+              .split('/').slice(0, -1)
+            arrayElementPath.push(`instrumentation/${sourceFileName}`)
+            let newPath = arrayElementPath.join('/')
+            imp.getSourceFile().insertStatements(0, `import { ${sourceFunctionName}Instrumented as ${sourceFunctionName} } from '${newPath.slice(1)}'`)
           }
-        )
-        arrayImportRef.remove()
-      } else {
-        file.insertStatements(0, `import { ${sourceFunctionName}Instrumented as ${sourceFunctionName} } from './instrumentation/${sourceFunctionName}'`)
-      }
+        })
+      imp.remove()
     })
   }
 
