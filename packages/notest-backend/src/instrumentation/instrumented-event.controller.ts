d6c8dd2dbeb0e48e87775be8db88d94d920197a7
Emanuele <emanuele.antimiani@gmail.com>
2022-05-04 19:09:04 +0200

    changed path of endpoint and added log

--- packages/notest-backend/src/instrumentation/instrumented-event.controller.ts
+++ packages/notest-backend/src/instrumentation/instrumented-event.controller.ts
@@ -2,13 +2,14 @@ import {Body, Controller, Post} from '@nestjs/common';
 import {InstrumentedFunctionService} from './instrumented-function.service';
 import {InstrumentedFunctionEvent} from "@butopen/notest-model";
 
-@Controller()
+@Controller('api')
 export class InstrumentedEventController {
   constructor(private readonly senderService: InstrumentedFunctionService) {
   }
 
   @Post('instrumented-function-event')
   async instrumentedFunctionEvent(@Body() events: InstrumentedFunctionEvent[]) {
+    console.log('received packet:' + events.length)
     await this.senderService.bulkSave(events)
     return {ok: true};
   }
