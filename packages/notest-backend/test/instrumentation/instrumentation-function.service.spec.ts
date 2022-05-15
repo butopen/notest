5ebb9d9e91bf726ddb83f5a1acf00943b8ef4d3a
Emanuele <emanuele.antimiani@gmail.com>
2022-05-15 11:32:58 +0200

    modified test to add events in db and trigger generation of test

--- packages/notest-backend/test/instrumentation/instrumentation-function.service.spec.ts
+++ packages/notest-backend/test/instrumentation/instrumentation-function.service.spec.ts
@@ -2,7 +2,10 @@ import {InstrumentedEvent} from "@butopen/notest-model";
 import {InstrumentedService} from "../../src/instrumentation/instrumented.service";
 import {DBConfig, PostgresDbService} from "../../src/postgres/postgres-db.service";
 
-describe("Test add event to DB", () => {
+const fetch = (url: RequestInfo, init?: RequestInit) =>
+  import('node-fetch').then(({default: fetch}) => fetch(url, init));
+
+describe("Test add event to DB and geneartion test", () => {
   const testConfig: DBConfig = {
     "host": "localhost",
     "user": "postgres",
@@ -10,33 +13,49 @@ describe("Test add event to DB", () => {
     "database": "testnt",
     "port": 5432
   }
+  const db: PostgresDbService = new PostgresDbService(testConfig)
+  const instFunction = new InstrumentedService(db)
 
-  const instFunction = new InstrumentedService(new PostgresDbService(testConfig))
-
-  test("add two events", () => {
+  test("trigger test generation", async () => {
+    await instFunction.generateTable();
     const eventsToAdd: InstrumentedEvent[] = []
+    for (let i = 0; i <= 2; i++) {
+      eventsToAdd.push({
+        script: "function",
+        file: "test/fileTest.ts",
+        function: "functionTest",
+        line: 0,
+        timestamp: Date.now() + i,
+        type: "input",
+        value: i
+      })
 
-    eventsToAdd.push({
-      script: "method",
-      file: "file",
-      function: "function",
-      line: 0,
-      timestamp: Date.now(),
-      type: "variable",
-      value: 7
-    })
-
-    eventsToAdd.push({
-      script: "method",
-      file: "file",
-      function: "function2",
-      line: 1,
-      timestamp: Date.now(),
-      type: "exception",
-      value: 7,
-      other: {"errore": 1}
-    })
+      eventsToAdd.push({
+        script: "function",
+        file: "test/fileTest.ts",
+        function: "functionTest",
+        line: 1,
+        timestamp: Date.now() + i,
+        type: "output",
+        value: i + 1
+      })
+    }
     let ids = []
-    instFunction.bulkSave(eventsToAdd).then(id => ids = id)
+    await instFunction.bulkSave(eventsToAdd).then(id => ids = id)
+    console.log(ids)
+
+    const rawResponse = await fetch("http://localhost:3000/test-generator/generate-test", {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        scriptType: "function",
+        filePath: "src/fileTest.ts",
+        functionName: "functionTest"
+      })
+    });
+    console.log(rawResponse.ok)
   })
 })
