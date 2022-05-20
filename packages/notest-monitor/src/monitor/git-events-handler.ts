b8730e4738e3dcc4d952e7ab44a62cdeec2c0ef6
Emanuele <emanuele.antimiani@gmail.com>
2022-05-20 11:41:11 +0200

    test coverage for git-handler

--- packages/notest-monitor/src/monitor/git-events-handler.ts
+++ packages/notest-monitor/src/monitor/git-events-handler.ts
@@ -9,16 +9,20 @@ export class GitEventsHandler {
 
   async getIdxsFromDiff(path: string) {
     const changes = await this.git.diff(['-U0', path])
-    return changes
-      .split('@@')
-      .filter((value, index) => index % 2 == 1)
-      .map(val => val.split('+')[1].replace(' ', ''))
-      .map(elem => {
-        const values = elem.split(',')
-        if (!values[1]) {
-          values[1] = '0'
-        }
-        return {start: parseInt(values[0]), end: parseInt(values[0]) + parseInt(values[1])}
-      })
+    return getIdxsFromString(changes)
   }
+}
+
+export function getIdxsFromString(changes: string) {
+  return changes
+    .split('@@')
+    .filter((value, index) => index % 2 == 1)
+    .map(val => val.split('+')[1].replace(' ', ''))
+    .map(elem => {
+      const values = elem.split(',')
+      if (!values[1]) {
+        values[1] = '0'
+      }
+      return {start: parseInt(values[0]), end: parseInt(values[0]) + parseInt(values[1])}
+    })
 }
--- packages/notest-monitor/test/git-handler/git-handler.test.ts
+++ packages/notest-monitor/test/git-handler/git-handler.test.ts
@@ -1,16 +1,13 @@
-import simpleGit from "simple-git";
+import {getIdxsFromString} from "../../src/monitor/git-events-handler";
 
 test("git handler", async () => {
-  const git = simpleGit()
-  const changes = await git.diff(['-U0', 'C:\\Users\\Emanuele\\Desktop\\UNI\\Tesi\\Progetto\\notest\\packages\\notest-monitor\\test\\esempi\\test-class.ts'])
-  const array = changes
-    .split('@@')
-    .filter((value, index) => index % 2 == 1)
-    .map(val => val.split('+')[1].replace(' ', ''))
-    .map(elem => {
-      const values = elem.split(',')
-      return {start: parseInt(values[0]), end: parseInt(values[0]) + parseInt(values[1])}
-    })
-  console.log(array)
-  console.log(changes)
+  const changes = "@@ -1,2 +3,4 @@ .. @@ -1,2 +3,4 @@..@@ -1,2 +3,4 @@..@@ -1,2 +3,4 @@..@@ -1,2 +3,4 @@"
+  expect(getIdxsFromString(changes)).toEqual(
+    [
+      {start: 3, end: 7},
+      {start: 3, end: 7},
+      {start: 3, end: 7},
+      {start: 3, end: 7},
+      {start: 3, end: 7}
+    ])
 })
